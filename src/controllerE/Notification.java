245a6539cf8634d4e77331e75b88b34ced18b301
khacnhat2k3 <khacnhat2k3@gmail.com>
2024-04-30 22:07:15 +0700

    chagne

--- src/controllerE/Notification.java
+++ src/controllerE/Notification.java
@@ -1,5 +1,37 @@
 package controllerE;
 
-public class Notification {
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+public class Notification extends Controller  {
+	@FXML
+    public void HomeE(ActionEvent event)throws IOException {
+    	super.HomeE(event);
+    }
+	
+	@FXML
+    public void settingE(ActionEvent event)throws IOException{
+    	super.settingE(event);
+    }
+	@FXML
+    public void calendarE(ActionEvent event)throws IOException{
+    	super.calendarE(event);
+    }
+	
+	@FXML
+    public void notificationE(ActionEvent event)throws IOException{
+    	super.notificationE(event);
+    }
+	
+	@FXML
+    public void MyAccountE(ActionEvent event)throws IOException{
+    	super.MyAccountE(event);
+    }
+	
+
+	 @FXML
+	    public void logout(ActionEvent event)throws IOException {
+	    	super.logout(event);
+	 }
 }
